ee60d12366991a335d7ca60db9dc0e76bc703537
Ismayilgit24 <ismayilibrahinzad9@gmail.com>
2024-11-22 02:04:23 +0400

    Initial commit

---
+++ ProniaApplication/ProniaApplication/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ProniaApplication.DAL;
+
+namespace ProniaApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDBContext>(
+                opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+
+                );
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=index}/{id?}"
+                );
+
+            app.Run();
+        }
+    }
+}
